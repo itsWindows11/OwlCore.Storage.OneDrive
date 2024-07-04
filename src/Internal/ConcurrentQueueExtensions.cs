365a54c2387704e98658433f621c1113937bbf2c
SimpleBear <81253203+itsWindows11@users.noreply.github.com>
2024-07-04 16:30:38 +0300

    Made `ConcurrentQueue<T>` clearing extension internal

--- src/Internal/ConcurrentQueueExtensions.cs
+++ src/Internal/ConcurrentQueueExtensions.cs
@@ -4,7 +4,7 @@ namespace OwlCore.Storage.OneDrive.Internal;
 
 internal static class ConcurrentQueueExtensions
 {
-    public static void Clear<T>(this ConcurrentQueue<T> queue)
+    internal static void Clear<T>(this ConcurrentQueue<T> queue)
     {
         while (!queue.IsEmpty)
         {
